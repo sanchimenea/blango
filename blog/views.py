035dac248512bb25ae1b66b796cc4c601bcdb4c3
Joseph Hanna <joseph.com.ec@gmail.com>
2024-01-29 04:05:30 +0000

    Finish custom filters

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
+from django.utils import timezone
+from blog.models import Post
 from django.shortcuts import render
 
-def index(request): 
-  return render(request, "blog/index.html")
+def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
