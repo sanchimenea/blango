e2b02460b26e1bd37e98e44b31f785b4f69f640f
Joseph Hanna <joseph.com.ec@gmail.com>
2024-04-28 18:42:03 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -57,6 +57,7 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         'rest_framework',
+        'rest_framework.authtoken',
         'blango_auth',
         'blog',
     ]
@@ -212,6 +213,15 @@ class Dev(Configuration):
     ACCOUNT_EMAIL_REQUIRED = True
     ACCOUNT_USERNAME_REQUIRED = False
     ACCOUNT_AUTHENTICATION_METHOD = "email"
+
+    # DRF
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
 
 class Prod(Dev):
